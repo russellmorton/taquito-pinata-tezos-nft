6c7c55e1b1410a3df8531e2f4b8560d5774d3c64
russellmorton <russellmorton@users.noreply.github.com>
2021-10-12 22:56:34 -0700

    updates

--- backend/src/PinataKeys.ts
+++ backend/src/PinataKeys.ts
@@ -1,4 +1,4 @@
 export default {
-  apiKey: "DUMMY_KEY",
-  apiSecret: "DUMMY_SECRET"
+  apiKey: "073f7df3f455230a4949",
+  apiSecret: "71f85bd71470df37d1141c72aa65af4e73f25cc17baae65fb71ee0d9e3e7c46d"
 };
